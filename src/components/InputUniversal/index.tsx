86b1b0d04b1fa1ebe3879600f7820a68b09d994e
Denis <unrealgoodandyou@mail.ru>
2024-01-26 17:03:29 +0300

    added a change in intup color

--- src/components/InputUniversal/index.tsx
+++ src/components/InputUniversal/index.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import { InputEnum } from "../../types/enums/InputEnum";
-import "./style.css";
 import PasswordIcon from "../../assets/icon/lock-solid.svg?react";
+import PasswordGreenIcon from "../../assets/icon/lock-green.svg?react";
+import PasswordGrayIcon from "../../assets/icon/lock-gray.svg?react";
 import EmailIcon from "../../assets/icon/email-solid.svg?react";
+import EmailGreenIcon from "../../assets/icon/email-green.svg?react";
+import EmailGrayIcon from "../../assets/icon/email-gray.svg?react";
+import EyeSlashGreenPassword from "../../assets/icon/eye-slash-green.svg?react";
 import EyeSlashPassword from "../../assets/icon/eye-slash-solid.svg?react";
-// import EyePassword from "../../assets/icon/eye-solid.svg?react";
+import EyeSlashGrayPassword from "../../assets/icon/eye-slash-gray.svg?react";
+import { CustomInput, IconWrapper, ContainerWrapper } from "./styled";
 
 interface InputType {
   type?: InputEnum;
@@ -13,52 +18,125 @@ interface InputType {
 function InputUniversal(props: InputType) {
   const { type = InputEnum.default } = props;
   const [isFocused, setIsFocused] = useState(false);
+  const [isInputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
 
   return (
-    <div className="icon-input-container">
+    <ContainerWrapper $isFocused={isFocused}>
       {type === InputEnum.password && (
         <>
-          <div className="container-icon">
-            <PasswordIcon width={20} height={20} />
-          </div>
-          <input
+          {isInputValue ? (
+            <>
+              {isFocused ? (
+                <IconWrapper>
+                  <PasswordGreenIcon width={20} height={20} />
+                </IconWrapper>
+              ) : (
+                <IconWrapper>
+                  <PasswordIcon width={20} height={20} />
+                </IconWrapper>
+              )}
+            </>
+          ) : isFocused ? (
+            <IconWrapper>
+              <PasswordGreenIcon width={20} height={20} />
+            </IconWrapper>
+          ) : (
+            <IconWrapper>
+              <PasswordGrayIcon width={20} height={20} />
+            </IconWrapper>
+          )}
+          <CustomInput
+            $isFocused={isFocused}
+            $isDefault={false}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
             style={{ border: 0 }}
             placeholder="Password"
             type={"password"}
-            onClick={() => {
-              setIsFocused(true);
-            }}
+            value={isInputValue}
+            onChange={handleInputChange}
           />
-          {isFocused ? (
-            <div className="container-icon">
-              <EyeSlashPassword width={20} height={20} />
-            </div>
+          {isInputValue ? (
+            isFocused ? (
+              <IconWrapper>
+                <EyeSlashGreenPassword width={20} height={20} />
+              </IconWrapper>
+            ) : (
+              <IconWrapper>
+                <EyeSlashPassword width={20} height={20} />
+              </IconWrapper>
+            )
+          ) : isFocused ? (
+            <IconWrapper>
+              <EyeSlashGreenPassword width={20} height={20} />
+            </IconWrapper>
           ) : (
-            <div className="container-icon">
-              <EyeSlashPassword width={20} height={20} />
-            </div>
+            <IconWrapper>
+              <EyeSlashGrayPassword width={20} height={20} />
+            </IconWrapper>
           )}
         </>
       )}
       {type === InputEnum.email && (
         <>
-          <div className="container-icon">
-            <EmailIcon width={20} height={20} />
-          </div>
-          <input style={{ border: 0 }} placeholder="Email" type={"email"} />
-          <div className="container-icon">
-            <EmailIcon width={20} height={20} />
-          </div>
+          {isInputValue ? (
+            isFocused ? (
+              <IconWrapper>
+                <EmailGreenIcon width={20} height={20} />
+              </IconWrapper>
+            ) : (
+              <IconWrapper>
+                <EmailIcon width={20} height={20} />
+              </IconWrapper>
+            )
+          ) : isFocused ? (
+            <IconWrapper>
+              <EmailGreenIcon width={20} height={20} />
+            </IconWrapper>
+          ) : (
+            <IconWrapper>
+              <EmailGrayIcon width={20} height={20} />
+            </IconWrapper>
+          )}
+          <CustomInput
+            $isFocused={isFocused}
+            $isDefault={true}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+            style={{ border: 0 }}
+            placeholder="Email"
+            type={"email"}
+            value={isInputValue}
+            onChange={handleInputChange}
+          />
         </>
       )}
       {type === InputEnum.default && (
         <>
-          <PasswordIcon width={20} height={20} />
-          <input className="input" placeholder="text" type={type} />
-          <PasswordIcon width={20} height={20} />
+          {isFocused ? (
+            <IconWrapper>
+              <EmailIcon width={20} height={20} />
+            </IconWrapper>
+          ) : (
+            <IconWrapper>
+              <EmailIcon width={20} height={20} />
+            </IconWrapper>
+          )}
+          <CustomInput
+            $isFocused={isFocused}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+            style={{ border: 0 }}
+            placeholder="Email"
+            type={type}
+          />
         </>
       )}
-    </div>
+    </ContainerWrapper>
   );
 }
 
---
+++ src/components/InputUniversal/styled.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const ContainerWrapper = styled.div<{ $isFocused: boolean }>`
+  border-radius: 10px;
+  display: flex;
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  height: 7vh;
+  border: 1px solid ${(props) => (props.$isFocused ? "green" : "gray")};
+  background: ${(props) => (props.$isFocused ? "#ebfaf1" : "#fafafa")};
+  overflow: hidden;
+`;
+
+export const IconWrapper = styled.div<{ $isInputEmpty?: boolean }>`
+  display: flex;
+  margin: 0 auto;
+  width: 15%;
+  vertical-align: middle;
+  justify-content: center;
+  align-items: center;
+  color: ${(props) => (props.$isInputEmpty ? "grey" : "black")};
+`;
+
+export const CustomInput = styled.input<{
+  $isDefault?: boolean;
+  $isFocused: boolean;
+}>`
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  border: 0px;
+  width: ${({ $isDefault }) => ($isDefault ? "100" : "85")}%;
+  background: ${(props) => (props.$isFocused ? "#ebfaf1" : "#fafafa")};
+
+  &:focus {
+    outline: none;
+    border: 1px solid blue;
+`;
